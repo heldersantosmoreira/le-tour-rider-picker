74c51034eaa025240a17ea76e29d746ef02ee6e7
Hélder Moreira <heldersantos.moreira@gmail.com>
2020-08-30 07:27:43 +0100

    Don't ensure unlocked staged unless rider changed

--- models/pick.rb
+++ models/pick.rb
@@ -5,11 +5,12 @@ class Pick < ActiveRecord::Base
   belongs_to :stage
   belongs_to :rider
 
-  validates :rider, :user, :stage,  presence: { message: 'is invalid' }
+  validates :rider, :user, :stage, presence: { message: 'is invalid' }
   validate :ensure_unlocked_stage
   validates_numericality_of :score, allow_nil: true
 
   def ensure_unlocked_stage
+    return unless rider_id_changed?
     return unless stage&.locked?
 
     errors.add(:stage, 'is invalid because it is locked')
