646a4b73fc49a3217cb201ed30d41a5f172b6be7
Hélder Moreira <heldersantos.moreira@gmail.com>
2020-09-01 22:15:38 +0100

    Update stages seeds

--- db/seeds/stages_2020.rb
+++ db/seeds/stages_2020.rb
@@ -1,10 +1,10 @@
 require_relative '../../models/stage'
 
-START_DATE = DateTime.new(2020, 8, 29)
-END_DATE = DateTime.new(2020, 9, 20)
+START_DATE = DateTime.new(2020, 8, 29, 15, 0)
+END_DATE = DateTime.new(2020, 9, 20, 15, 0)
 REST_DAYS = [
-  DateTime.new(2020, 9, 7),
-  DateTime.new(2020, 9, 14),
+  DateTime.new(2020, 9, 7, 15, 0),
+  DateTime.new(2020, 9, 14, 15, 0),
 ].freeze
 
 ((START_DATE..END_DATE).to_a - REST_DAYS).each_with_index do |date, i|
